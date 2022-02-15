f6a46db66ff73966a1e661905aa0d3041e3fb69a
ChallenAi <fengyiai@gmail.com>
2022-02-15 21:44:42 +0800

    implement find interface for horm

--- horm.go
+++ horm.go
@@ -10,7 +10,11 @@ import (
 	"github.com/challenai/horm/utils"
 )
 
-const HBaseTagHint = "hbase"
+const (
+	HBaseTagHint = "hbase"
+	ModelName    = "Model"
+	ResultSize   = 32 // todo: customized batchSize
+)
 
 // base model for every hbase model
 type Model struct {
@@ -31,6 +35,17 @@ type schema struct {
 	field2col []string
 }
 
+type Column struct {
+	Family    string
+	Name      string
+	Timestamp int64
+}
+
+type Filter struct {
+	FilterString string
+	// Limit        int64
+}
+
 // Model should implement Table interface to specify the namespace and table name.
 type Table interface {
 	Namespace() string
@@ -47,26 +62,129 @@ func NewDB(client *hbase.THBaseServiceClient) *DB {
 }
 
 // HBase rows range query
-func (h *DB) Find() *DB {
+func (h *DB) Find(ctx context.Context, list interface{}, startRow, stopRow string, selects []Column, filter *Filter) *DB {
+	// border case: input a nil as model, not allowed
+	if list == nil {
+		panic("can't input nil as a model")
+	}
+	if reflect.TypeOf(list).Kind() != reflect.Ptr {
+		panic("list should be a slice of struct pointer, for example: *[]User")
+	}
+	if reflect.TypeOf(list).Elem().Kind() != reflect.Slice {
+		panic("list should be a slice of struct pointer, for example: *[]User")
+	}
+	modelType := reflect.TypeOf(list).Elem().Elem()
+	if modelType.Kind() != reflect.Struct {
+		panic("list should be a slice of struct pointer, for example: *[]User")
+	}
+	model := reflect.New(modelType).Interface()
+	tb, ok := model.(Table)
+	if !ok {
+		panic("please set namespace and table name for this model")
+	}
+
+	tScan := &hbase.TScan{
+		StartRow: []byte(startRow),
+		StopRow:  []byte(stopRow),
+	}
+	if selects != nil && len(selects) > 0 {
+		tScan.Columns = make([]*hbase.TColumn, 0, len(selects))
+		for _, v := range selects {
+			col := &hbase.TColumn{
+				Family:    []byte(v.Family),
+				Qualifier: []byte(v.Name),
+			}
+			if v.Timestamp != 0 {
+				ts := int64(v.Timestamp)
+				col.Timestamp = &ts
+			}
+			tScan.Columns = append(tScan.Columns, col)
+		}
+	}
+	if filter != nil {
+		if filter.FilterString != "" {
+			tScan.FilterString = []byte(filter.FilterString)
+		}
+		// if filter.limit > 0 {
+		// 	limit := int32(filter.Limit)
+		// 	tScan.Limit = &limit
+		// }
+	}
+
+	var scanResults []*hbase.TResult_
+	listValue := reflect.ValueOf(list).Elem()
+	for {
+		var lastResult *hbase.TResult_ = nil
+		currentResults, err := h.db.GetScannerResults(ctx, []byte(fmt.Sprintf("%s:%s", tb.Namespace(), tb.TableName())), tScan, ResultSize)
+		if err != nil {
+			h.Error = err
+			return h
+		}
+		for _, tResult := range currentResults {
+			lastResult = tResult
+			scanResults = append(scanResults, tResult)
+		}
+		if lastResult == nil {
+			break
+		} else {
+			nextStartRow := getClosestRowAfter(lastResult.Row)
+			tScan.StartRow = nextStartRow
+		}
+	}
+	for _, v := range scanResults {
+		m := reflect.New(modelType)
+		mPtr := m.Elem()
+		h.retrieveValue(&mPtr, v)
+		listValue.Set(reflect.Append(listValue, m.Elem()))
+	}
 	return h
 }
 
-// parse imported model so that we don't need to parse all the model fields everytime.
-func (h *DB) parseModel(model interface{}) schema {
-	schm := schema{}
-	ptrVals := reflect.ValueOf(model)
-	if ptrVals.Kind() != reflect.Ptr {
-		panic("can't parse model type bacause model is not a pointer")
+func getClosestRowAfter(row []byte) []byte {
+	var nextRow []byte
+	var i int
+	for i = 0; i < len(row); i++ {
+		nextRow = append(nextRow, row[i])
 	}
-	vals := ptrVals.Elem()
-	h.schemas[reflect.TypeOf(model).Name()] = schm
-	schm.field2col = make([]string, vals.NumField())
+	nextRow = append(nextRow, 0x00)
+	return nextRow
+}
+
+func (h *DB) retrieveValue(value *reflect.Value, result *hbase.TResult_) {
+	schm, ok := h.schemas[value.Type().Name()]
+	if !ok {
+		schm = h.registerModel(*value)
+	}
+	base := &Model{
+		Rowkey: string(result.Row),
+	}
+	value.FieldByName(ModelName).Set(reflect.ValueOf(base))
+	for _, v := range result.ColumnValues {
+		key := fmt.Sprintf("%s:%s", v.Family, v.Qualifier)
+		if idx, ok := schm.col2field[key]; ok {
+			field := value.Field(idx)
+			// fmt.Println(field.SetString())
+			switch field.Kind() {
+			case reflect.Int:
+				field.Set(reflect.ValueOf(utils.DecodeInt(v.GetValue())))
+				// field.SetInt(int64(utils.DecodeInt(v.GetValue())))
+			case reflect.String:
+				field.SetString(string(v.GetValue()))
+			}
+		}
+	}
+}
+
+// parse imported model so that we don't need to parse all the model fields everytime.
+func (h *DB) registerModel(values reflect.Value) schema {
+	schm := schema{}
+	schm.field2col = make([]string, values.NumField())
 	schm.col2field = map[string]int{}
-	for i := 0; i < vals.Type().NumField(); i++ {
-		if vals.Type().Field(i).Name == "Model" {
+	for i := 0; i < values.Type().NumField(); i++ {
+		if values.Type().Field(i).Name == "Model" {
 			continue
 		}
-		tagsList := strings.Split(vals.Type().Field(i).Tag.Get(HBaseTagHint), ",")
+		tagsList := strings.Split(values.Type().Field(i).Tag.Get(HBaseTagHint), ",")
 		if len(tagsList) < 2 {
 			panic("hbase column doesn't have column family or qualifier")
 		}
@@ -74,6 +192,7 @@ func (h *DB) parseModel(model interface{}) schema {
 		schm.field2col[i] = name
 		schm.col2field[name] = i
 	}
+	h.schemas[values.Type().Name()] = schm
 	return schm
 }
 
@@ -88,35 +207,20 @@ func (h *DB) Get(ctx context.Context, model interface{}, rowkey string) *DB {
 		panic("please set namespace and table name for this model")
 	}
 
-	schm, ok := h.schemas[reflect.TypeOf(model).Name()]
-	if !ok {
-		schm = h.parseModel(model)
-	}
-
 	result, err := h.db.Get(ctx, []byte(fmt.Sprintf("%s:%s", tb.Namespace(), tb.TableName())), &hbase.TGet{Row: []byte(rowkey)})
 	if err != nil {
 		h.Error = err
 		return h
 	}
 
-	fields := reflect.ValueOf(model).Elem()
-	for _, v := range result.ColumnValues {
-		key := fmt.Sprintf("%s:%s", v.Family, v.Qualifier)
-		if idx, ok := schm.col2field[key]; ok {
-			field := fields.Field(idx)
-			switch field.Kind() {
-			case reflect.Int:
-				field.Set(reflect.ValueOf(utils.DecodeInt(v.GetValue())))
-			case reflect.String:
-				field.Set(reflect.ValueOf(string(v.GetValue())))
-			}
-		}
-	}
+	value := reflect.ValueOf(model).Elem()
+	h.retrieveValue(&value, result)
+
 	return h
 }
 
 // insert or update model to HBase
-func (h *DB) Set(ctx context.Context, model interface{}) *DB {
+func (h *DB) Set(ctx context.Context, model interface{}, rowkey string) *DB {
 	// border case: input a nil as model, not allowed
 	if model == nil {
 		panic("can't input nil as a model")
